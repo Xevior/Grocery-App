3937da14a354557fbabcb00ab0a6ab8f6f6e9a2b
xvior <francisxevior@gamil.com>
2022-11-08 13:36:20 +0300

    first commit

---
+++ app/src/main/java/com/dhruva/shopping/AdminCategoryActivity.java
@@ -0,0 +1,97 @@
+package com.dhruva.shopping;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+    private ImageView tShirts, sportsTShirts, femaleDresses, sweathers;
+
+    private Button LogoutBtn, CheckOrdersBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+
+
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        tShirts = (ImageView) findViewById(R.id.t_shirts);
+        sportsTShirts = (ImageView) findViewById(R.id.sports_t_shirts);
+        femaleDresses = (ImageView) findViewById(R.id.female_dresses);
+        sweathers = (ImageView) findViewById(R.id.sweathers);
+
+
+
+
+        tShirts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.dhruva.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "tShirts");
+                startActivity(intent);
+            }
+        });
+        sportsTShirts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.dhruva.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Sports tShirts");
+                startActivity(intent);
+            }
+        });
+
+
+        femaleDresses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.dhruva.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Female Dresses");
+                startActivity(intent);
+            }
+        });
+
+
+        sweathers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.dhruva.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Sweathers");
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
